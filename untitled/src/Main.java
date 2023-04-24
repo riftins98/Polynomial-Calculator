c035aafec7fe79b73eef914c0260d5036b0ed320
riftins98 <128430526+riftins98@users.noreply.github.com>
2023-04-24 23:53:46 +0300

    Delete untitled directory

--- untitled/src/Main.java
+++
@@ -1,7 +0,0 @@
-import java.lang.reflect.Array;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- untitled/src/Scalar.java
+++
@@ -1,7 +0,0 @@
-public interface Scalar {
-    Scalar add(Scalar s);
-    Scalar mul(Scalar s);
-    Scalar neg();
-    Scalar power(int exponent);
-    int sign();
-}
